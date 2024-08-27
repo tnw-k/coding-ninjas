4d4b130620108f29efaef4728ab0f583d1bc12e7
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-27 23:02:55 +0530

    feat: Add Events component to display past events on Home page

---
+++ components/Events/Events.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Timeline from './PastEvents'
+
+function Events() {
+  return (
+    <div className="flex-col justify-center">
+<h1 className="text-center text-3xl font-bold text-white">      Our Events </h1>
+<Timeline/>
+    </div>
+  )
+}
+
+export default Events
