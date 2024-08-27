36c20f25f1fdf5f931e74af492755ab9d3615f1f
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-28 00:16:39 +0530

    style: Update Events component layout and font size

--- components/Events/Events.jsx
+++ components/Events/Events.jsx
@@ -3,8 +3,8 @@ import Timeline from './PastEvents'
 
 function Events() {
   return (
-    <div className="flex-col justify-center">
-<h1 className="text-center text-3xl font-bold text-white">      Our Events </h1>
+    <div className="flex-col my-20 justify-center">
+<h1 className="text-center text-6xl font-bold title-animation ">      Our Events </h1>
 <Timeline/>
     </div>
   )
