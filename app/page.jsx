55902fb2141f01bdee507aba58c027e182528dc9
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-27 23:05:32 +0530

    feat: Add lazy loading for images on Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
 import Events from "@components/Events/Events"
-import Timeline from "@components/Events/PastEvents"
 import Hero from "@components/Hero"
 
 const Home = () => {
   return (
     <div className="w-full text-white">
         <Hero />
+        <Events />
     </div>
   )
 }
