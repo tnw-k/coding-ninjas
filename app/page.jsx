6161bdba5a6d3d3e25ce572f81fe6dd1044742cc
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-27 22:55:08 +0530

    chore: Add Timeline component to Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
+import Timeline from "@components/Events/PastEvents"
 import Hero from "@components/Hero"
 
 const Home = () => {
   return (
-    <div className="w-full text-white">
+    <div className="w-full  ">
         {/* <Hero /> */}
+<div className="flex justify-center p-4">
+<Timeline/></div>
     </div>
   )
 }
