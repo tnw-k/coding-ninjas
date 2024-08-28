300338dfd5a49429206bb500109ecb136c679e7a
tnw-k <2205606@kiit.ac.in>
2024-08-28 22:34:05 +0530

    Desc in Hero Section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -23,13 +23,14 @@ const Hero = () => {
         <div 
             className="w-full z-20 md:px-40 px-10 h-screen absolute text-center flex-col gap-6 top-0 flex justify-center items-center ">
             <div className="">
-                <h1 className="title-animation title-outline  outline-2 cursor-default red-hat-display-heading font-semibold text-[90px] mb-10 
-                bg-gradient-to-br from-white via-[#fcaf7c] to-[#fe6702] bg-clip-text text-transparent 
-                ">Coding Ninjas:<br/>
-                KIIT Chapter</h1>
+                <h1 className="title-animation title-outline bg-gradient-to-br from-orange-200 via-[#ff7f29] to-[#fe6702] bg-clip-text text-transparent outline-2 cursor-default red-hat-display-heading font-semibold text-7xl mb-10">
+                  {/* bg-gradient-to-br from-white via-[#fcaf7c] to-[#fe6702] bg-clip-text text-transparent  */}
+                  Coding Ninjas:<br/>
+                  KIIT Chapter
+                </h1>
             </div>
-            <p className="text-opacity-60 text-xl text-white cursor-default sm:mx-52">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, atque quia doloremque aut accusamus ipsa maiores.</p>
-            <button className="red-hat-display-heading explore-btn cursor-pointer text-[#F17522] rounded-xl px-4 py-2  bg-[#141819] border-4 text-2xl border-[#F17522]">Explore More</button>            
+            <p className="text-opacity-60 text-xl text-white cursor-default sm:mx-52">Empowering students to have fun while learning, coding, and developing together across diverse tech and creative domains.</p>
+            <button className="red-hat-display-heading explore-btn cursor-pointer text-[#F17522] rounded-xl px-4 py-2  bg-[#141819] border-4 text-xl border-[#F17522]">Explore More</button>            
         </div>
     </section>
   )
