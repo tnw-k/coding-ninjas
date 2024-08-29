4a62183ba5b602d3b527ff7bdf960db1c1a77462
Kanika <kanikasingh7482@gmail.com>
2024-08-29 17:02:57 +0530

    nav2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="flex-row items-center justify-center relative bg-cover bg-no-repeat bg-center  text-white overflow-hidden" style={{ backgroundImage: "url('/puzzle-background.png')" , height:'150px' , backgroundColor:'black'}}>
-      <div className="container mx-auto flex items-center justify-between py-4 relative z-20">
+    <nav className="flex-row items-center justify-center bg-black relative bg-cover bg-no-repeat bg-center text-white overflow-hidden" style={{ height: '100px' }}>
+      <div className="container mx-auto flex items-center justify-between relative z-20" style={{ marginTop: '-20px' }}> 
         <Link href="/" className="flex items-center">
-          <Image src="/logo.png" alt="Logo" width={200} height={200} className="mr-4 pl-10 z-30 relative" />
+          <Image src="/logo.png" alt="Logo" width={150} height={150} className="mr-2 z-30 relative ml-5" />
         </Link>
         <div className="flex justify-center flex-1 space-x-4 nav-items z-10">
           <NavItem href="/" text="Home" />
@@ -23,14 +23,17 @@ const Navbar = () => {
 };
 
 const NavItem = ({ href, text }) => (
-  <Link href={href} className="group relative px-2 py-1.5">
-    <div className="absolute inset-0 w-full h-full transition-all duration-300 ease-in-out  opacity-0 group-hover:opacity-40"></div>
+  <Link href={href} className="group relative px-1 py-1">
+    <div className="absolute inset-0 w-full h-full transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-40"></div>
     <div className="absolute top-0 left-0 w-full h-[2px] bg-white -translate-y-1 opacity-0 transition-all duration-300 ease-in-out group-hover:translate-y-0 group-hover:opacity-70"></div>
     <div className="absolute bottom-0 left-0 w-full h-[2px] bg-white translate-y-1 opacity-0 transition-all duration-300 ease-in-out group-hover:translate-y-0 group-hover:opacity-70"></div>
-    <div className="relative z-10   md:text-base lg:text-lg xl:text-xl 2xl:text-[1vw]  transition-colors duration-300 ease-in-out">
+    <div className="relative z-10 md:text-sm lg:text-base xl:text-lg transition-colors duration-300 ease-in-out">
       {text}
     </div>
   </Link>
 );
 
 export default Navbar;
+
+
+
