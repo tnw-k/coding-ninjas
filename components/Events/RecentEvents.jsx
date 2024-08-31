b7c631790d23a02046327543f0e8c3e9b2a8addb
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-31 13:47:16 +0530

    refactor: Update RecentEvents component UI for better readability and consistency

--- components/Events/RecentEvents.jsx
+++ components/Events/RecentEvents.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { FaClock, FaMapMarkerAlt, FaCalendarAlt } from 'react-icons/fa';
 import Image from 'next/image';
+import Link from 'next/link';
+
 const badges = [
-  { label: 'MERN' },
-  { label: 'AI' },
-  { label: 'DSA' },
-  { label: 'Soft skills' },
+  { label: 'AWS' },
+  { label: 'WEB' },
+  { label: 'DATABASE' },
+  { label: 'APIs' },
 ];
 
 const RecentEvents = () => {
@@ -37,18 +39,16 @@ const RecentEvents = () => {
   });
 
   return (
-    <div className="rounded-lg mt-20 p-2  border-2 border-[#F66C3B] px-8 shadow-sm bg-Advertise xl:mx-20">
+    <div className="rounded-lg mt-20 p-2  border-2 border-[#F66C3B] sm:px-4 px-8 lg:px-8 shadow-sm bg-Advertise xl:mx-20">
       <div className="sm:flex-row flex flex-col items-center justify-between mb-4">
-        <div className="flex flex-col lg:w-[600px] sm:w-[400px]">
+        <div className="flex flex-col lg:w-[600px] sm:w-[390px]">
           <div className="flex items-center mb-4">
             <span className=""><Image src="/assets/images/logo.png" alt="Coding Ninjas" className="mr-4" width={50} height={50}></Image></span>
             <h2 className="text-3xl font-bold text-orange-600">Badlav</h2>
           </div>
           <p className="text-gray-800 lg:text-[16px] sm:text-sm  font-semibold text-justify">
-            Complete             Complete CS Education to transform you into a 10X Developer
-            Complete CS Education to transform you into a 10X Developer
-            Complete CS Education to transform you into a 10X Developer
-            CS Education to transform you into a 10X Developer
+
+Witness the evolution of cloud technologies over the decades. 🌩️🚀 From the early days of basic cloud storage to today's advanced, scalable solutions, explore how innovation has transformed the way we store, manage, and process data. Dive into the journey of technological progress and see how cloud computing continues to revolutionize industries worldwide.
           </p>
           <div className=" gap-2 grid-cols-4 grid mt-5">
             {badges.map((badge, index) => (
@@ -59,17 +59,17 @@ const RecentEvents = () => {
           </div>
         </div>
         <div className="text-center mt-8 flex flex-col ">
-        <button className="bg-orange-500 text-white text-lg sm:w-60 w-full  h-12  border-2 border-orange-500 rounded-lg font-bold shadow hover:bg-transparent hover:text-orange-600 transition">
+     <Link href="https://docs.google.com/forms/d/e/1FAIpQLSdb2ngjQyrfG0gUYj_X9IKyenQ6Yn2udK0zWXxKSCZPNFIa5A/viewform">   <button className="bg-orange-500 text-white text-lg sm:w-60 w-full  h-12  border-2 border-orange-500 rounded-lg font-bold shadow hover:bg-transparent hover:text-orange-600 transition">
           Join Event
-        </button>
+        </button> </Link>
        <div className="flex  justify-around mt-6">
        <div className="flex text-black  items-center space-x-1 font-semibold">
           <FaCalendarAlt className="text-orange-600" />
           <span>01 Sep 2024</span>
         </div>
-        <div className="flex text-black  items-center space-x-1 font-semibold">
+        <div className="flex text-black sm:ml-3  items-center space-x-0.5 font-semibold">
           <FaMapMarkerAlt className="text-green-500" />
-          <span>Venue</span>
+          <span>Campus 15 Block A</span>
         </div>
        </div>
         <div className="flex justify-center items-center mt-3 space-x-1">
