c59e1d6108ef7390311304af27fe40f999c09028
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-31 13:47:00 +0530

    refactor: Update Events component UI for better readability and consistency

--- components/Events/Events.jsx
+++ components/Events/Events.jsx
@@ -3,7 +3,7 @@ import Timeline from './PastEvents'
 
 function Events() {
   return (
-    <div id="events" className="flex-col my-20 justify-center xl:px-20 sm:px-10 lg:px-20 px-6">
+    <div id="events" className="flex-col my-20 mt-28 justify-center xl:px-20 sm:px-10 lg:px-20 px-6">
 <h1 className="text-center sm:text-5xl text-4xl font-bold event-title ">      Our Events </h1>
 <div classNam="flex justify-center mx-auto">
 <Timeline/>
