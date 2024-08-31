5f632d9fd3a5a1165b2e21fb59ca257f39304d30
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-31 12:21:58 +0530

    refactor: Update RecentEvents component UI for better readability and consistency

--- components/Events/RecentEvents.jsx
+++ components/Events/RecentEvents.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { FaClock, FaMapMarkerAlt, FaCalendarAlt } from 'react-icons/fa';
 import Image from 'next/image';
 const badges = [
-  { label: 'MERN Stack' },
+  { label: 'MERN' },
   { label: 'AI' },
   { label: 'DSA' },
   { label: 'Soft skills' },
@@ -37,14 +37,14 @@ const RecentEvents = () => {
   });
 
   return (
-    <div className="rounded-lg mt-20 p-2  border-2 border-[#F66C3B] px-8 shadow-sm bg-Advertise mx-20">
-      <div className="flex items-center justify-between mb-4">
-        <div className="flex flex-col w-[600px]">
-          <div className="flex items-center">
-            <span className=""><Image src="/logo.png" alt="Coding Ninjas" width={70} height={70}></Image></span>
-            <h2 className="text-2xl font-bold text-orange-600">Coding Ninjas TechVarsity</h2>
+    <div className="rounded-lg mt-20 p-2  border-2 border-[#F66C3B] px-8 shadow-sm bg-Advertise xl:mx-20">
+      <div className="sm:flex-row flex flex-col items-center justify-between mb-4">
+        <div className="flex flex-col lg:w-[600px] sm:w-[400px]">
+          <div className="flex items-center mb-4">
+            <span className=""><Image src="/assets/images/logo.png" alt="Coding Ninjas" className="mr-4" width={50} height={50}></Image></span>
+            <h2 className="text-3xl font-bold text-orange-600">Badlav</h2>
           </div>
-          <p className="text-gray-800 text-sm font-medium">
+          <p className="text-gray-800 lg:text-[16px] sm:text-sm  font-semibold text-justify">
             Complete             Complete CS Education to transform you into a 10X Developer
             Complete CS Education to transform you into a 10X Developer
             Complete CS Education to transform you into a 10X Developer
@@ -52,14 +52,14 @@ const RecentEvents = () => {
           </p>
           <div className=" gap-2 grid-cols-4 grid mt-5">
             {badges.map((badge, index) => (
-              <div key={index} className=" py-1 px-2 rounded-lg flex items-center justify-center bg-orange-100 text-orange-500 text-sm border-orange-600 border font-medium">
+              <div key={index} className=" sm:py-1 sm:px-2 px-0.5 py-0.5 rounded-lg flex items-center justify-center bg-orange-100 text-orange-500 sm:text-sm text-[12px] border-orange-600 border font-medium">
                 <span>{badge.label}</span>
               </div>
             ))}
           </div>
         </div>
-        <div className="text-center mt-8 ">
-        <button className="bg-orange-500 text-white text-lg w-60 h-12  border-2 border-orange-500 rounded-lg font-bold shadow hover:bg-transparent hover:text-orange-600 transition">
+        <div className="text-center mt-8 flex flex-col ">
+        <button className="bg-orange-500 text-white text-lg sm:w-60 w-full  h-12  border-2 border-orange-500 rounded-lg font-bold shadow hover:bg-transparent hover:text-orange-600 transition">
           Join Event
         </button>
        <div className="flex  justify-around mt-6">
